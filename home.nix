7ae3aeb101d6405ede397b89fd8643d332c7a3b0
Gallophostrix <gallophostrix@proton.me>
2025-10-14 21:56:03 +0200

    Initial NixOS + HM config

---
+++ home.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+
+{
+  # User information
+  home.username = "mik";
+  home.homeDirectory = "/home/mik";
+
+  # Configuration version
+  home.stateVersion = "25.05";
+
+  programs.home-manager.enable = true;
+
+  # Programs
+  programs.git = {
+    enable = true;
+    userName = "Gallophostrix";
+    userEmail = "gallophostrix@proton.me";
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window.opacity = 0.9;
+    };
+  };
+
+  programs.fish.enable = true;
+  
+  home.sessionVariables.SHELL = "${pkgs.fish}/bin/fish";
+
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+}
