3196cb3421029784754793fcbd4e4e9f8edf8fc2
Gallophostrix <gallophostrix@proton.me>
2025-10-18 15:19:29 +0200

    Total refactoring correction fish hm

--- modules/core/fish.nix
+++ modules/core/fish.nix
@@ -1,47 +1,38 @@
-{ lib, pkgs, ... }:
-let
-  inherit (lib) getExe getExe';
-  eza        = getExe pkgs.eza;
-  microfetch = getExe pkgs.microfetch;
-  trashPut   = getExe' pkgs.trash-cli "trash-put";
-  trashRest  = getExe' pkgs.trash-cli "trash-restore";
-in
+{ config, lib, pkgs, ... }:
 {
   programs.fish = {
     enable = true;
 
-    # Simple aliases (HM turns these into fish aliases)
+    # Aliases
     shellAliases = {
-      l    = "${eza} -lh --icons=auto";
-      ls   = "${eza} -1 --icons=auto";
-      ll   = "${eza} -lha --icons=auto --sort=name --group-directories-first";
-      ld   = "${eza} -lhD --icons=auto";
-      tree = "${eza} --icons=auto --tree";
+      l    = "eza -lh --icons=auto";
+      ls   = "eza -1 --icons=auto";
+      ll   = "eza -lha --icons=auto --sort=name --group-directories-first";
+      ld   = "eza -lhD --icons=auto";
+      tree = "eza --icons=auto --tree";
       vc   = "codium --disable-gpu";
-      nf   = "${microfetch}";
+      nf   = "microfetch";
       cp   = "cp -iv";
       mv   = "mv -iv";
       rm   = "rm -vI";
       bc   = "bc -ql";
       mkd  = "mkdir -pv";
-      tp   = "${trashPut}";
-      tpr  = "${trashRest}";
+      tp   = "trash-put";
+      tpr  = "trash-restore";
       grep = "grep --color=always";
-      "list-gens" = "nixos-rebuild list-generations";
-      "update-input" = "nix flake update";
-      sysup = "nix flake update --flake ~/nixcfg && rebuild";
-      games = "cd /mnt/games/";
-      work  = "cd /mnt/work/";
-      media = "cd /mnt/work/media/";
-      projects = "cd /mnt/work/Projects/";
-      proj  = "cd /mnt/work/Projects/";
-      dev   = "cd /mnt/work/Projects/";
+      "list-gens"   = "nixos-rebuild list-generations";
+      "update-input"= "nix flake update";
+      sysup         = "nix flake update --flake ~/nixcfg && rebuild";
+      games   = "cd /mnt/games/";
+      work    = "cd /mnt/work/";
+      media   = "cd /mnt/work/media/";
+      projects= "cd /mnt/work/Projects/";
+      proj    = "cd /mnt/work/Projects/";
+      dev     = "cd /mnt/work/Projects/";
     };
 
-    # Raw fish code goes here
     interactiveShellInit = ''
       if status is-interactive
-        # direnv & zoxide hooks
         if type -q direnv
           direnv hook fish | source
         end
@@ -49,11 +40,9 @@ in
           zoxide init fish | source
         end
 
-        # Key bindings
         bind \ca beginning-of-line
         bind \ce end-of-line
 
-        # Options / history
         set -g fish_autosuggestion yes
         set -g fish_history_size 100000
         set -g fish_history "$XDG_DATA_HOME/fish/history"
@@ -70,34 +59,31 @@ in
         set -g fish_inc_append_history yes
         set -g fish_share_history yes
 
-        # Env vars
         set -gx XMONAD_CONFIG_DIR (set -q XDG_CONFIG_HOME; or echo ~/.config)/xmonad
         set -gx XMONAD_DATA_DIR   (set -q XDG_DATA_HOME;  or echo ~/.local/share)/xmonad
         set -gx XMONAD_CACHE_DIR  (set -q XDG_CACHE_HOME; or echo ~/.cache)/xmonad
         set -gx FZF_DEFAULT_OPTS "--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
-      end
 
-      # Functions/aliases that need Fish syntax (kept as-is)
-      function UUID; uuidgen | tr -d \n; end
-      alias lf 'set tmp (mktemp); command lf -last-dir-path="$tmp" $argv; if test -f "$tmp"; rm -f "$tmp"; set dir (cat $tmp); if test -d $dir; cd $dir; end'
-      alias fnew 'if test -d $argv[2]; echo "Directory $argv[2] already exists!"; return 1; end; nix flake new $argv[2] --template ~/N/dev-shells#$argv[1]; cd $argv[2]; direnv allow'
-      alias finit 'nix flake init --template ~/N/dev-shells#$argv[1]; direnv allow'
-      alias cdown 'set N $argv[1]; while test $N -gt 0; echo (figlet -c $N | lolcat); sleep 1; set N (math $N - 1); end'
-      alias tml "tmux list-sessions"
-      alias tma "tmux attach"
-      alias tms "tmux attach -t (tmux ls -F '#{session_name}: #{session_path} (#{session_windows} windows)' | fzf | cut -d: -f1)"
+        function UUID; uuidgen | tr -d \n; end
+        alias lf 'set tmp (mktemp); command lf -last-dir-path="$tmp" $argv; if test -f "$tmp"; rm -f "$tmp"; set dir (cat $tmp); if test -d $dir; cd $dir; end'
+        alias fnew 'if test -d $argv[2]; echo "Directory $argv[2] already exists!"; return 1; end; nix flake new $argv[2] --template ~/N/dev-shells#$argv[1]; cd $argv[2]; direnv allow'
+        alias finit 'nix flake init --template ~/N/dev-shells#$argv[1]; direnv allow'
+        alias cdown 'set N $argv[1]; while test $N -gt 0; echo (figlet -c $N | lolcat); sleep 1; set N (math $N - 1); end'
+        alias tml "tmux list-sessions"
+        alias tma "tmux attach"
+        alias tms "tmux attach -t (tmux ls -F '#{session_name}: #{session_path} (#{session_windows} windows)' | fzf | cut -d: -f1)"
+      end
     '';
   };
 
-  # Let HM do the right thing for these:
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-  };
+  environment.systemPackages = with pkgs; [
+    eza trash-cli microfetch figlet lolcat fzf tmux lf direnv zoxide
+  ];
+
+  # Auto hooks
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
   programs.zoxide.enable = true;
 
-  # Make sure tools used by your aliases are present
-  home.packages = with pkgs; [
-    eza trash-cli microfetch figlet lolcat fzf tmux lf
-  ];
+  systemd.user.startServices = "sd-switch";
 }
