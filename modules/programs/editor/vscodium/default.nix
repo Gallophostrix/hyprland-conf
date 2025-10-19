f91a50a69cc1298e0368cade988de2b51e594d08
gallophostrix <gallophostrix@proton.me>
2025-10-20 00:32:08 +0200

    vscodium refactoring

--- modules/programs/editor/vscodium/default.nix
+++ modules/programs/editor/vscodium/default.nix
@@ -7,28 +7,28 @@
         package = pkgs.vscodium;
         profiles.default = {
           extensions = with pkgs.vscode-extensions; [
-            eamodio.gitlens
-            github.vscode-github-actions
+            # --- Workflow ---
+            # eamodio.gitlens
+            # github.vscode-github-actions
+
+            # --- Formatting ---
+            charliermarsh.ruff
+            redhat.vscode-yaml
+            jnoortheen.nix-ide
             yzhang.markdown-all-in-one
+            tamasfe.even-better-toml
+            # rust-lang.rust-analyzer
+            # ms-python.python
+
+            # --- Tools ---
+            esbenp.prettier-vscode
+            christian-kohler.path-intellisense
+
+            # --- Themes ---
             catppuccin.catppuccin-vsc
             catppuccin.catppuccin-vsc-icons
-            # asvetliakov.vscode-neovim
-            # vscodevim.vim
-            # tamasfe.even-better-toml
-            jnoortheen.nix-ide
-            # redhat.vscode-yaml
-            # vadimcn.vscode-lldb
-            # rust-lang.rust-analyzer
-            # ms-vscode.cpptools
-            # ms-vscode.cmake-tools
-            # ms-vscode.makefile-tools
-            # ziglang.vscode-zig
-            # ms-dotnettools.csharp
-            # ms-python.python
-            # pkief.material-icon-theme
-            # equinusocio.vsc-material-theme
-            # dracula-theme.theme-dracula
           ];
+
           keybindings = [
             {
               key = "ctrl+q";
@@ -41,23 +41,66 @@
             }
           ];
           userSettings = {
-            "update.mode" = "none";
-            # "extensions.autoUpdate" = false; # Fixes vscode freaking out when theres an update
-            "window.titleBarStyle" = "custom"; # needed otherwise vscode crashes, see https://github.com/NixOS/nixpkgs/issues/246509
+            # --- General ---
+            "workbench.startupEditor" = "none";
+
+            "telemetry.telemetryLevel" = "off";
+            "security.workspace.trust.untrustedFiles" = "open";
+
+            # --- Workflow ---
+            "terminal.integrated.inheritEnv" = true;
+            "explorer.confirmDragAndDrop" = false;
+
+            "editor.codeActionsOnSave" = {
+              "source.organizeImports" = true;
+              "source.fixAll.ruff" = true;
+            };
+            "editor.formatOnSave" = true;
+            "editor.formatOnType" = false;
+            "editor.formatOnPaste" = true;
+
+            "breadcrumbs.enabled" = true;
+            "workbench.layoutControl.enabled" = false;
+
+            "workbench.editor.limit.enabled" = true;
+            "workbench.editor.limit.value" = 10;
+            "workbench.editor.limit.perEditorGroup" = true;
+            "explorer.openEditors.visible" = 0;
+
+            "path-intellisense.autoSlashAfterDirectory" = true;
+            "path-intellisense.extensionOnImport" = true;
+            "path-intellisense.showHiddenFiles" = false;
+
+            # --- Visuals ---
+            "window.titleBarStyle" = "custom";
             "window.menuBarVisibility" = "classic";
             # "window.zoomLevel" = 0.5;
             "editor.fontSize" = 14;
-            "workbench.colorTheme" = "Catppuccin Mocha";
-            "workbench.iconTheme" = "catppuccin-mocha";
-            "catppuccin.accentColor" = "mauve";
-            "vsicons.dontShowNewVersionMessage" = false;
-            "explorer.confirmDragAndDrop" = false;
-            "editor.fontLigatures" = true;
-            "workbench.startupEditor" = "none";
-            "telemetry.enableCrashReporter" = false;
-            "telemetry.enableTelemetry" = false;
+            "editor.wordWrap" = "on";
 
-            "security.workspace.trust.untrustedFiles" = "open";
+            "editor.semanticHighlighting" = "configuredByTheme";
+            "editor.inlineSuggest.enabled" = true;
+            "editor.stickyScroll.enabled" = true;
+            "editor.renderControlCharacters" = false;
+            "editor.mouseWheelZoom" = true;
+
+            "editor.minimap.enabled" = false;
+            "workbench.sideBar.location" = "left";
+            # "workbench.activityBar.location" = "hidden";
+            # "workbench.editor.showTabs" = "single";
+            # "workbench.statusBar.visible" = false;
+            "workbench.layoutControl.type" = "menu";
+
+            "editor.scrollbar.verticalScrollbarSize" = 2;
+            "editor.scrollbar.horizontalScrollbarSize" = 2;
+            "editor.scrollbar.vertical" = "hidden";
+            "editor.scrollbar.horizontal" = "hidden";
+
+            "workbench.colorTheme" = "Catppuccin Mocha";
+            "workbench.iconTheme" = "catppuccin-vsc-icons";
+            "catppuccin.accentColor" = "mauve";
+            # "vsicons.dontShowNewVersionMessage" = false;
+            "editor.fontLigatures" = true;
 
             "git.enableSmartCommit" = true;
             "git.autofetch" = true;
@@ -65,7 +108,31 @@
             "gitlens.hovers.annotations.changes" = false;
             "gitlens.hovers.avatars" = false;
 
-            # --- Nix formatting ---
+            "[javascript]" = {"editor.defaultFormatter" = "esbenp.prettier-vscode";};
+            "[typescript]" = {"editor.defaultFormatter" = "esbenp.prettier-vscode";};
+            "[json]" = {"editor.defaultFormatter" = "esbenp.prettier-vscode";};
+            "[html]" = {"editor.defaultFormatter" = "esbenp.prettier-vscode";};
+            "[css]" = {"editor.defaultFormatter" = "esbenp.prettier-vscode";};
+            "[markdown]" = {"editor.defaultFormatter" = "esbenp.prettier-vscode";};
+
+            # --- Language config ---
+            "[python]" = {
+              "editor.defaultFormatter" = "charliermarsh.ruff";
+              "editor.formatOnSave" = true;
+            };
+            "ruff.enable" = true;
+
+            "[yaml]" = {
+              "editor.defaultFormatter" = "redhat.vscode-yaml";
+            };
+            "yaml.format.enable" = true;
+            "yaml.validate" = true;
+            "yaml.schemaStore.enable" = true;
+
+            "[nix]" = {
+              "editor.defaultFormatter" = "jnoortheen.nix-ide";
+            };
+            "nix.formatterPath" = "alejandra";
             "nix.enableLanguageServer" = true;
             "nix.serverPath" = "nixd";
             "nix.serverSettings" = {
@@ -76,241 +143,13 @@
               };
             };
 
-            "[nix]" = {
-              "editor.defaultFormatter" = "jnoortheen.nix-ide";
-              "editor.formatOnSave" = true;
+            "markdown.extension.toc.updateOnSave" = true;
+            "markdown.extension.list.indentationSize" = "inherit";
+            "markdown.preview.doubleClickToSwitchToEditor" = false;
 
-              "editor.codeActionsOnSave" = {};
-            };
-
-            "editor.semanticHighlighting.enabled" = true;
-            "gopls" = {
-              "ui.semanticTokens" = true;
-            };
-
-            "editor.codeActionsOnSave" = {
-              # "source.organizeImports" = "explicit";
-            };
-            "editor.inlineSuggest.enabled" = true;
-            "editor.formatOnSave" = true;
-            "editor.formatOnType" = true;
-            "editor.formatOnPaste" = true;
-
-            "editor.minimap.enabled" = false;
-            "workbench.sideBar.location" = "left";
-            # "workbench.activityBar.location" = "hidden";
-            # "workbench.editor.showTabs" = "single";
-            # "workbench.statusBar.visible" = false;
-            "workbench.layoutControl.type" = "menu";
-            "workbench.editor.limit.enabled" = true;
-            "workbench.editor.limit.value" = 10;
-            "workbench.editor.limit.perEditorGroup" = true;
-            "explorer.openEditors.visible" = 0;
-            "breadcrumbs.enabled" = true;
-            "editor.renderControlCharacters" = false;
-            "editor.stickyScroll.enabled" = false; # Top code preview
-            "editor.scrollbar.verticalScrollbarSize" = 2;
-            "editor.scrollbar.horizontalScrollbarSize" = 2;
-            "editor.scrollbar.vertical" = "hidden";
-            "editor.scrollbar.horizontal" = "hidden";
-            "workbench.layoutControl.enabled" = false;
-
-            "editor.mouseWheelZoom" = true;
-
-            "C_Cpp.autocompleteAddParentheses" = true;
-            "C_Cpp.formatting" = "vcFormat";
-            "C_Cpp.vcFormat.newLine.closeBraceSameLine.emptyFunction" = true;
-            "C_Cpp.vcFormat.newLine.closeBraceSameLine.emptyType" = true;
-            "C_Cpp.vcFormat.space.beforeEmptySquareBrackets" = true;
-            "C_Cpp.vcFormat.newLine.beforeOpenBrace.block" = "sameLine";
-            "C_Cpp.vcFormat.newLine.beforeOpenBrace.function" = "sameLine";
-            "C_Cpp.vcFormat.newLine.beforeElse" = false;
-            "C_Cpp.vcFormat.newLine.beforeCatch" = false;
-            "C_Cpp.vcFormat.newLine.beforeOpenBrace.type" = "sameLine";
-            "C_Cpp.vcFormat.space.betweenEmptyBraces" = true;
-            "C_Cpp.vcFormat.space.betweenEmptyLambdaBrackets" = true;
-            "C_Cpp.vcFormat.indent.caseLabels" = true;
-            "C_Cpp.intelliSenseCacheSize" = 2048;
-            "C_Cpp.intelliSenseMemoryLimit" = 2048;
-            "C_Cpp.default.browse.path" = [
-              ''''${workspaceFolder}/**''
-            ];
-            "C_Cpp.default.cStandard" = "gnu11";
-            "C_Cpp.inlayHints.parameterNames.hideLeadingUnderscores" = false;
-            "C_Cpp.intelliSenseUpdateDelay" = 500;
-            "C_Cpp.workspaceParsingPriority" = "medium";
-            "C_Cpp.clang_format_sortIncludes" = true;
-            "C_Cpp.doxygen.generatedStyle" = "/**";
-
-            "vim.leader" = "<Space>";
-            "vim.useCtrlKeys" = true;
-            "vim.hlsearch" = true;
-            "vim.useSystemClipboard" = true;
-            "vim.handleKeys" = {
-              "<C-f>" = true;
-              "<C-a>" = false;
-            };
-            "vim.insertModeKeyBindings" = [
-              {
-                "before" = [
-                  "k"
-                  "j"
-                ];
-                "after" = [
-                  "<Esc>"
-                  "l"
-                ];
-              }
-            ];
-            "vim.normalModeKeyBindingsNonRecursive" = [
-              # NAVIGATION
-              # switch b/w buffers
-              {
-                "before" = ["<S-h>"];
-                "commands" = [":bprevious"];
-              }
-              {
-                "before" = ["<S-l>"];
-                "commands" = [":bnext"];
-              }
-
-              # splits
-              {
-                "before" = [
-                  "leader"
-                  "v"
-                ];
-                "commands" = [":vsplit"];
-              }
-              {
-                "before" = [
-                  "leader"
-                  "s"
-                ];
-                "commands" = [":split"];
-              }
-
-              # panes
-              {
-                "before" = ["<C-h>"];
-                "commands" = ["workbench.action.focusLeftGroup"];
-              }
-              {
-                "before" = ["<C-j>"];
-                "commands" = ["workbench.action.focusBelowGroup"];
-              }
-              {
-                "before" = ["<C-k>"];
-                "commands" = ["workbench.action.focusAboveGroup"];
-              }
-              {
-                "before" = ["<C-l>"];
-                "commands" = ["workbench.action.focusRightGroup"];
-              }
-              # NICE TO HAVE
-              {
-                "before" = [
-                  "leader"
-                  "w"
-                ];
-                "commands" = [":w!"];
-              }
-              {
-                "before" = [
-                  "leader"
-                  "q"
-                ];
-                "commands" = [":q!"];
-              }
-              {
-                "before" = [
-                  "leader"
-                  "x"
-                ];
-                "commands" = [":x!"];
-              }
-              {
-                "before" = [
-                  "["
-                  "d"
-                ];
-                "commands" = ["editor.action.marker.prev"];
-              }
-              {
-                "before" = [
-                  "];"
-                  "d"
-                ];
-                "commands" = ["editor.action.marker.next"];
-              }
-              {
-                "before" = [
-                  "<leader>"
-                  "c"
-                  "a"
-                ];
-                "commands" = ["editor.action.quickFix"];
-              }
-              /*
-                {
-                "before" = [":"];
-                "commands" = ["workbench.action.showCommands"];
-              }
-              */
-              {
-                "before" = [
-                  "<leader>"
-                  "f"
-                ];
-                "commands" = ["workbench.action.quickOpen"];
-              }
-              {
-                "before" = ["<C-n>"];
-                "commands" = ["workbench.action.toggleSidebarVisibility"];
-              }
-              {
-                "before" = [
-                  "<leader>"
-                  "p"
-                ];
-                "commands" = ["editor.action.formatDocument"];
-              }
-              {
-                "before" = [
-                  "g"
-                  "h"
-                ];
-                "commands" = ["editor.action.showDefinitionPreviewHover"];
-              }
-            ];
-            "vim.visualModeKeyBindings" = [
-              # Stay in visual mode while indenting
-              {
-                "before" = ["<"];
-                "commands" = ["editor.action.outdentLines"];
-              }
-              {
-                "before" = [">"];
-                "commands" = ["editor.action.indentLines"];
-              }
-              # Move selected lines while staying in visual mode
-              {
-                "before" = ["J"];
-                "commands" = ["editor.action.moveLinesDownAction"];
-              }
-              {
-                "before" = ["K"];
-                "commands" = ["editor.action.moveLinesUpAction"];
-              }
-              # toggle comment selection
-              {
-                "before" = [
-                  "leader"
-                  "c"
-                ];
-                "commands" = ["editor.action.commentLine"];
-              }
-            ];
+            "[toml]" = {"editor.defaultFormatter" = "tamasfe.even-better-toml";};
+            "evenBetterToml.formatter.ruler" = 100;
+            "evenBetterToml.formatter.alignEntries" = true;
           };
         };
       };
