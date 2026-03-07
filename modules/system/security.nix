9a6417499e7c47923ee59d5664c8d0a74e3132e4
gallophostrix <gallophostrix@proton.me>
2026-03-07 11:34:26 +0100

    fix(keyring): replacing gnome with keepassxc

--- modules/system/security.nix
+++ modules/system/security.nix
@@ -7,15 +7,6 @@
       killUnconfinedConfinables = true;
       packages = [pkgs.apparmor-profiles];
     };
-    pam.services.hyprlock = {
-      text = ''
-        auth      sufficient pam_fprintd.so
-        auth      include    system-login
-        account   include    system-login
-        password  include    system-login
-        session   include    system-login
-      '';
-    };
   };
 
   services.fprintd = {
@@ -25,8 +16,6 @@
       driver = pkgs.libfprint-2-tod1-goodix;
     };
   };
-
-  services.gnome.gnome-keyring.enable = true;
 
   sops = {
     defaultSopsFile = ../../secrets/secrets.yaml;
